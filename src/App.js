9bb9958865f93da1369f580587e40f6fefe6af45
emamyari <emamyari@yahoo.com>
2022-08-28 18:40:05 +0430

    simple react app was developed in class.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Counter from './components/counter'
+import Menu from './components/menu';
+class App extends Component {
+  state = {  } 
+  render() { 
+    return (<div>     
+              <Menu></Menu>
+              <Counter></Counter>
+              <Counter></Counter>
+             </div>);
+  }
+}
+ 
+export default App;
---
+++ src/components/counter.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <div>
+                <button >+</button>
+                <span>10</span>
+                <button >-</button>
+            </div>
+         );
+    }
+}
+ 
+export default Counter;
