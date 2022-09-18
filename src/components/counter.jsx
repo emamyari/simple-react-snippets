e3b5667e6caf654f659de1826583c0212bd4abf8
emamyari <emamyari@yahoo.com>
2022-09-18 18:05:21 +0430

    change some func.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,18 +2,21 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = { 
-        count:100,
-        names:['ali reza','reza','mohammad']
+        count:0
     } 
     handleInc =() =>{
         this.setState({count: this.state.count+1})
     }
+    handleDec =() =>{
+        this.setState({count: this.state.count-1})
+    }
+    
     render() { 
         let myClass="badge m-2 badge-"
         myClass=myClass+(this.state.count>0?'warning':'primary');
         return (
             <div>
-                <button className='btn btn-warning'>Decrement</button>
+                <button onClick={this.handleDec} className='btn btn-warning'>Decrement</button>
                 <span className={myClass}>{this.countText()}</span>
                 <button onClick={this.handleInc} className='btn btn-success'>Increment</button>
                 <button className='btn btn-danger'>Delete</button>
