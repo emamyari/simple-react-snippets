9eb5ecb7dcd3005fc10723298e856dfe427dbf48
emamyari <emamyari@yahoo.com>
2022-09-18 18:30:13 +0430

    change project structure.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Counter from './components/counter'
+import Counters from './components/counters';
 import Menu from './components/menu';
 class App extends Component {
   state = {  } 
   render() { 
     return (<div>     
              
-              <Counter></Counter>
+            <Counters></Counters>
+            
+            
              </div>);
   }
 }
