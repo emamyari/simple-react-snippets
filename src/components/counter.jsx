2bda6bf2428526152134df1d16368b746c5664c3
emamyari <emamyari@yahoo.com>
2023-01-24 18:29:56 +0330

    apply dynamic class to badges

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -19,16 +19,20 @@ class Counter extends Component {
     render() { 
         let myClass=''
         this.state.count===0?myClass='bg-danger col-md-1 btn ': myClass='bg-light col-md-1 btn '
+        let myClass2=''
+        let col=this.props.color
+        this.state.count===0?myClass2='  col-md-1 btn '+ col: myClass2=' col-md-1 btn '+col
+        
         return (<div>
 
 
                     <span className={myClass}> {this.props.id}    </span>
-
                     <button onClick={this.handleInc} className='btn btn-success m-2'>افزایش</button>
                     <span className={myClass}>{this.state.count}</span>
                     <button onClick={this.handleDec} className='btn btn-warning' >کاهش</button>
                     <button className='btn btn-danger'>حذف</button>
                     <span className={myClass}> {this.props.title}    </span>
+                    <span className={myClass2} > {this.props.color}    </span>
 
                 </div>);
     }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,19 +3,19 @@ import Counter from './counter';
 
 class Counters extends Component {
     state = {
-            myList:[{id:1,name:'iphone',q:10},
-                    {id:2,name:'airPod',q:5},
-                    {id:3,name:'TV',q:100},
-                    {id:4,name:'adaptor',q:5},
-                    {id:5,name:'laptop',q:2},
-                    {id:5,name:'watch',q:9},
-                    {id:5,name:'ipad',q:8},
+            myList:[{id:1,name:'iphone',q:10,color:'red'},
+                    {id:2,name:'airPod',q:5,color:'yellow'},
+                    {id:3,name:'TV',q:100,color:'pink'},
+                    {id:4,name:'adaptor',q:5,color:'white'},
+                    {id:5,name:'laptop',q:2,color:'black'},
+                    {id:5,name:'watch',q:9,color:'black'},
+                    {id:5,name:'ipad',q:8,color:'blue'},
                     ]
             }
     render() {
         return (
         <div>
-           {this.state.myList.map(item=><Counter id={item.id} tedad={item.q} title={item.name}></Counter>)}  
+           {this.state.myList.map(item=><Counter color={item.color} id={item.id} tedad={item.q} title={item.name}></Counter>)}  
 
         </div>
         );
