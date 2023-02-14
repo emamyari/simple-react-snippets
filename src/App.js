b51f02309ded9b08b06c10a6c5a2692d25f7ac04
emamyari <emamyari@yahoo.com>
2023-02-14 18:20:23 +0330

    change state position from counters to app js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
 import Counters from './components/counters'
-function App() {
-  return (<div>
-       <Counters></Counters>
-   </div>
-  );
+import Nav from './components/nav'
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    myList: [{ id: 1, name: 'iphone', q: 10, color: 'red' },
+    { id: 2, name: 'airPod', q: 5, color: 'yellow' },
+    { id: 3, name: 'TV', q: 100, color: 'pink' },
+    { id: 4, name: 'adaptor', q: 5, color: 'white' },
+    { id: 5, name: 'laptop', q: 2, color: 'black' },
+    { id: 6, name: 'watch', q: 9, color: 'black' },
+    { id: 7, name: 'ipad', q: 8, color: 'blue' },
+    ]
+  }
+
+  handleDec = (c) => {
+    let pos = this.state.myList.indexOf(c)
+    let first = this.state.myList.slice(0, pos)
+    c.q = c.q - 1
+    let last = this.state.myList.slice(pos + 1)
+    let finalList = [...first, c, ...last]
+    this.setState({ myList: finalList })
+  }
+  handleDel = (c) => {
+    let finalList = this.state.myList.filter(item => item != c)
+    this.setState({ myList: finalList })
+  }
+  handleInc = (c) => {
+
+    let pos = this.state.myList.indexOf(c)
+    let first = this.state.myList.slice(0, pos)
+    c.q = c.q + 1
+    let last = this.state.myList.slice(pos + 1)
+    let finalList = [...first, c, ...last]
+    this.setState({ myList: finalList })
+
+  }
+  render() {
+    return (
+      <div>
+        <Nav tedad={this.state.myList.length}></Nav>
+        <Counters 
+          handleDel={this.handleDel}
+          handleInc={this.handleInc}
+          handleDec={this.handleDec}
+          counters={this.state.myList}
+        ></Counters>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,52 +2,18 @@ import React, { Component } from 'react';
 import Counter from './counter';
 
 class Counters extends Component {
-    state = {
-            myList:[{id:1,name:'iphone',q:10,color:'red'},
-                    {id:2,name:'airPod',q:5,color:'yellow'},
-                    {id:3,name:'TV',q:100,color:'pink'},
-                    {id:4,name:'adaptor',q:5,color:'white'},
-                    {id:5,name:'laptop',q:2,color:'black'},
-                    {id:6,name:'watch',q:9,color:'black'},
-                    {id:7,name:'ipad',q:8,color:'blue'},
-                    ]
-            }
-            
-handleDec=(c)=>{
-    let pos=this.state.myList.indexOf(c)
-    let first=this.state.myList.slice(0,pos)
-    c.q=c.q-1
-    let last=this.state.myList.slice(pos+1)
-    let finalList=[...first,c,...last]
-    this.setState({myList:finalList})
-}
-handleDel=(c)=>{
-    let finalList=this.state.myList.filter(item=>item!=c)
-    this.setState({myList:finalList})
-}
 
-
-handleInc=(c)=>{
-    
-    let pos=this.state.myList.indexOf(c)
-    let first=this.state.myList.slice(0,pos)
-    c.q=c.q+1
-    let last=this.state.myList.slice(pos+1)
-    let finalList=[...first,c,...last]
-    this.setState({myList:finalList})
-
-}
     render() {
         return (
         <div>
-           {this.state.myList.map(item=><Counter 
+           {this.props.counters.map(item=><Counter 
                                         color={item.color} 
                                         id={item.id} 
                                         tedad={item.q} 
                                         title={item.name} 
-                                        hinc={this.handleInc} 
-                                        hdec={this.handleDec}
-                                        hdel={this.handleDel}
+                                        hinc={this.props.handleInc} 
+                                        hdec={this.props.handleDec}
+                                        hdel={this.props.handleDel}
                                         cntr={item}></Counter>)}  
 
         </div>
